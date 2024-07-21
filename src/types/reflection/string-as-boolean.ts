31cbd6669488ac5f9d4ace752774dacf1fea5ad6
SvS-NickM <svs.nickm@outlook.com>
2024-07-21 16:15:30 +0300

    feat: implemented DeepOmit
    - implemented ChangeTypeOf
    - comments and renaming

--- src/types/reflection/string-as-boolean.ts
+++ src/types/reflection/string-as-boolean.ts
@@ -1,3 +1,12 @@
+/**
+ * Parses {@link T_String} to into {@link boolean} literal
+ * 
+ * @description
+ * 
+ * 'true' => true
+ * 
+ * 'false' => false
+ */
 export type StringAsBoolean<T_String extends string> = 
 (
     T_String extends `${infer T_Value extends boolean}`
--- src/types/reflection/string-as-null.ts
+++ src/types/reflection/string-as-null.ts
@@ -1,3 +1,10 @@
+/**
+ * Parses {@link T_String} to into {@link null} literal
+ * 
+ * @description
+ * 
+ * 'null' => null
+ */
 export type StringAsNull<T_String extends string> = 
 (
     T_String extends `${infer T_Value extends null}`
